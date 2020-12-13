c7fc23d50a307027a44cf028332d5ae631f19db5
uni <68980965+CryptoUnico@users.noreply.github.com>
2020-12-13 15:14:23 -0600

    Version Control + Rewards and Burn Addresses

--- contracts/interfaces/IMerkleDistributor.sol
+++ contracts/interfaces/IMerkleDistributor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.5.0;
+pragma solidity =0.5.0;
 
 // Allows anyone to claim a token if they exist in a merkle root.
 interface IMerkleDistributor {
@@ -7,6 +7,11 @@ interface IMerkleDistributor {
     function token() external view returns (address);
     // Returns the merkle root of the merkle tree containing account balances available to claim.
     function merkleRoot() external view returns (bytes32);
+    // Returns the address of the rewards pool contributed to by this contract.
+    function rewardsAddress() external view returns (address);
+    // Returns the address of the burn pool contributed to by this contract.
+    function burnAddress() external view returns (address);
+
     // Returns true if the index has been marked claimed.
     function isClaimed(uint256 index) external view returns (bool);
     // Claim the given amount of the token to the given address. Reverts if the inputs are invalid.
